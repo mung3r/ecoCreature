ea4518cc86fb0e53cfa300f757f8f2d60ca81a20
mung3r <rodney@bighatchet.com>
2011-11-26 14:15:11 -0800

    fixed a message typo

--- src/main/java/se/crafted/chrisb/ecoCreature/managers/ecoMessageManager.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/managers/ecoMessageManager.java
@@ -17,7 +17,7 @@ public class ecoMessageManager implements Cloneable
     public final static String DEATH_PENALTY_MESSAGE = "&7You wake up to find &6<amt>&7 missing from your pockets!";
     public final static String PVP_REWARD_MESSAGE = "&7You are awarded &6<amt>&7 for murdering &5<crt>.";
 
-    public final static String NO_REWARD_MESSAGE = "'&7You slayed a &5<crt>&7 using a &3<itm>.";
+    public final static String NO_REWARD_MESSAGE = "&7You slayed a &5<crt>&7 using a &3<itm>.";
     public final static String REWARD_MESSAGE = "&7You are awarded &6<amt>&7 for slaying a &5<crt>.";
     public final static String PENALTY_MESSAGE = "&7You are penalized &6<amt>&7 for slaying a &5<crt>.";
 
