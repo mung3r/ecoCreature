8188f2c7a7ddc052cf6cb91de5b6a268b9684f72
mung3r <rodney@bighatchet.com>
2012-06-22 23:45:38 -0700

    cleaned up pull request

--- src/main/java/se/crafted/chrisb/ecoCreature/managers/ecoRewardManager.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/managers/ecoRewardManager.java
@@ -272,17 +272,12 @@ public class ecoRewardManager
     private Double computeReward(Player player, ecoReward reward)
     {
         Double amount = reward.getRewardAmount();
-        //Double groupAmount = 0D;
-        //Double timeAmount = 0D;
-        //Double envAmount = 0D;
-        Double TotalAmount = amount;
 
         try {
             if (ecoCreature.permission.getPrimaryGroup(player.getWorld().getName(), player.getName()) != null) {
                 String group = ecoCreature.permission.getPrimaryGroup(player.getWorld().getName(), player.getName()).toLowerCase();
                 if (hasPermission(player, "gain.group") && groupMultiplier.containsKey(group)) {
-                    //groupAmount = amount * groupMultiplier.get(group) - amount;
-                    TotalAmount = TotalAmount * groupMultiplier.get(group);
+                    amount *= groupMultiplier.get(group);
                 }
             }
         }
@@ -294,17 +289,14 @@ public class ecoRewardManager
         }
 
         if (hasPermission(player, "gain.time") && timeMultiplier.containsKey(ecoEntityUtil.getTimePeriod(player))) {
-            //timeAmount = amount * timeMultiplier.get(ecoEntityUtil.getTimePeriod(player)) - amount;
-            TotalAmount = TotalAmount * timeMultiplier.get(ecoEntityUtil.getTimePeriod(player)) 
+            amount *= timeMultiplier.get(ecoEntityUtil.getTimePeriod(player));
         }
 
         if (hasPermission(player, "gain.environment") && envMultiplier.containsKey(player.getWorld().getEnvironment())) {
-            //envAmount = amount * envMultiplier.get(player.getWorld().getEnvironment()) - amount;
-            TotalAmount = TotalAmount * envMultiplier.get(player.getWorld().getEnvironment())
+            amount *= envMultiplier.get(player.getWorld().getEnvironment());
         }
 
-        //Double total = amount + groupAmount + timeAmount + envAmount;
-        return isIntegerCurrency ? (double) Math.round(TotalAmount) : TotalAmount;
+        return isIntegerCurrency ? (double) Math.round(amount) : amount;
     }
 
     private Boolean hasPermission(Player player, String perm)
