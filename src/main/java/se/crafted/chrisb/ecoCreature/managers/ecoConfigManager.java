22a76a588dee5676865e2c5790a2e7df3aa2c452
mung3r <mung3r@bighatchet.com>
2012-08-09 10:49:01 -0700

    removed unused import

--- src/main/java/se/crafted/chrisb/ecoCreature/managers/ecoConfigManager.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/managers/ecoConfigManager.java
@@ -1,7 +1,6 @@
 package se.crafted.chrisb.ecoCreature.managers;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
