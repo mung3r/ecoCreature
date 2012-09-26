3d986daa3a6f6648126602182e6109300e49b371
mung3r <rodney@bighatchet.com>
2012-09-26 08:16:57 -0700

    added party reward messaging

--- src/main/java/se/crafted/chrisb/ecoCreature/events/listeners/RewardEventListener.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/events/listeners/RewardEventListener.java
@@ -3,8 +3,6 @@ package se.crafted.chrisb.ecoCreature.events.listeners;
 import java.util.HashSet;
 import java.util.Set;
 
-import net.milkbowl.vault.economy.Economy;
-
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.ExperienceOrb;
@@ -17,6 +15,7 @@ import se.crafted.chrisb.ecoCreature.ecoCreature;
 import se.crafted.chrisb.ecoCreature.commons.DependencyUtils;
 import se.crafted.chrisb.ecoCreature.commons.ECLogger;
 import se.crafted.chrisb.ecoCreature.events.RewardEvent;
+import se.crafted.chrisb.ecoCreature.messages.DefaultMessage;
 import se.crafted.chrisb.ecoCreature.messages.Message;
 import se.crafted.chrisb.ecoCreature.messages.MessageHandler;
 import se.crafted.chrisb.ecoCreature.messages.MessageToken;
@@ -24,6 +23,9 @@ import se.crafted.chrisb.ecoCreature.rewards.Reward;
 
 public class RewardEventListener implements Listener
 {
+    private static final String PARTY_REWARD_MESSAGE = "&7Party awarded &6<amt>&7.";
+    private static final String PARTY_PENALTY_MESSAGE = "&Party penalized &6<amt>&7.";
+
     private ecoCreature plugin;
 
     public RewardEventListener(ecoCreature plugin)
@@ -35,66 +37,101 @@ public class RewardEventListener implements Listener
     public void onRewardEvent(RewardEvent event)
     {
         if (!event.isCancelled()) {
-
+            Reward reward = event.getReward();
             Player player = event.getPlayer();
 
-            if (player != null) {
-                Reward reward = event.getReward();
+            dropCoin(player.getName(), reward);
+            dropItems(reward);
+            dropEntities(reward);
 
-                if (DependencyUtils.hasEconomy()) {
-                    double amount = reward.getCoin() * reward.getGain();
+            plugin.getMetrics().addCount(reward.getName());
+        }
+    }
 
-                    Set<String> party = new HashSet<String>();
-                    if (reward.getParty().size() > 0) {
-                        ECLogger.getInstance().debug("Amount to be divided with party: " + amount);
-                        party.addAll(reward.getParty());
-                        amount /= reward.getParty().size();
-                    }
-                    else {
-                        party.add(player.getName());
-                    }
+    private void dropCoin(String player, Reward reward)
+    {
+        if (!DependencyUtils.hasDeathTpPlus()) {
+            return;
+        }
 
-                    if (reward.isIntegerCurrency()) {
-                        ECLogger.getInstance().debug("Amount to be rounded: " + amount);
-                        amount = Math.round(amount);
-                    }
+        double amount = calculateAmount(reward);
 
-                    Economy economy = DependencyUtils.getEconomy();
+        Set<String> party = new HashSet<String>();
+        party.add(player);
+        party.addAll(reward.getParty());
 
-                    for (String member : party) {
-                        if (amount > 0.0) {
-                            economy.depositPlayer(member, amount);
-                        }
-                        else if (amount < 0.0) {
-                            economy.withdrawPlayer(member, Math.abs(amount));
-                        }
+        for (String member : party) {
+            registerAmount(member, amount);
 
-                        if (member.equals(player.getName())) {
-                            Message message = reward.getMessage();
-                            message.addParameter(MessageToken.PLAYER, member);
-                            message.addParameter(MessageToken.AMOUNT, economy.format(Math.abs(amount)));
+            Message message = member.equals(player) ? reward.getMessage() : getPartyMessage(amount);
+            message.addParameter(MessageToken.PLAYER, member);
+            message.addParameter(MessageToken.AMOUNT, DependencyUtils.getEconomy().format(Math.abs(amount)));
 
-                            MessageHandler handler = new MessageHandler(member, message);
-                            handler.send();
-                        }
-                        else {
-                            // TODO: send different message to party members
-                        }
-                    }
-                }
+            MessageHandler handler = new MessageHandler(member, message);
+            handler.send();
+        }
+    }
 
-                for (ItemStack stack : reward.getItemDrops()) {
-                    reward.getWorld().dropItemNaturally(reward.getLocation(), stack);
-                }
+    private double calculateAmount(Reward reward)
+    {
+        double amount = reward.getCoin() * reward.getGain();
+        ECLogger.getInstance().debug("Amount to be awarded: " + amount);
+        amount /= reward.hasParty() ? reward.getParty().size() : 1;
 
-                for (EntityType type : reward.getEntityDrops()) {
-                    Entity entity = reward.getWorld().spawn(reward.getLocation(), type.getEntityClass());
-                    if (entity instanceof ExperienceOrb) {
-                        ((ExperienceOrb) entity).setExperience(1);
-                    }
-                }
+        if (reward.getParty().size() > 1) {
+            amount /= reward.getParty().size();
+            ECLogger.getInstance().debug("Amount divided with party: " + amount);
+        }
 
-                plugin.getMetrics().addCount(reward.getName());
+        if (reward.isIntegerCurrency()) {
+            ECLogger.getInstance().debug("Amount to be rounded: " + amount);
+            amount = Math.round(amount);
+        }
+
+        return amount;
+    }
+
+    private void registerAmount(String member, double amount)
+    {
+        if (!DependencyUtils.hasEconomy()) {
+            return;
+        }
+
+        if (amount > 0.0) {
+            DependencyUtils.getEconomy().depositPlayer(member, amount);
+        }
+        else if (amount < 0.0) {
+            DependencyUtils.getEconomy().withdrawPlayer(member, Math.abs(amount));
+        }
+    }
+
+    private Message getPartyMessage(double amount)
+    {
+        Message message = new DefaultMessage("");
+
+        if (amount > 0.0) {
+            message = new DefaultMessage(PARTY_REWARD_MESSAGE);
+        }
+        else if (amount < 0.0) {
+            message = new DefaultMessage(PARTY_PENALTY_MESSAGE);
+        }
+
+        return message;
+    }
+
+    private void dropItems(Reward reward)
+    {
+        for (ItemStack stack : reward.getItemDrops()) {
+            reward.getWorld().dropItemNaturally(reward.getLocation(), stack);
+        }
+    }
+
+    private void dropEntities(Reward reward)
+    {
+        for (EntityType type : reward.getEntityDrops()) {
+            Entity entity = reward.getWorld().spawn(reward.getLocation(), type.getEntityClass());
+            if (entity instanceof ExperienceOrb) {
+                ((ExperienceOrb) entity).setExperience(1);
             }
         }
     }
