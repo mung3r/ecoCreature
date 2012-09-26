465db26e5c3db2358e8e3795ce7d28035c406646
mung3r <mung3r@bighatchet.com>
2012-09-25 17:52:14 -0700

    added coin constructor removed some unused methods

--- src/main/java/se/crafted/chrisb/ecoCreature/rewards/models/CoinDrop.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/rewards/models/CoinDrop.java
@@ -8,24 +8,20 @@ public class CoinDrop
     private DoubleRange range;
     private double percentage;
 
+    public CoinDrop(DoubleRange range, double percentage)
+    {
+        this.range = range;
+        this.percentage = percentage;
+    }
+
     public DoubleRange getRange()
     {
         return range;
     }
 
-    public void setRange(DoubleRange range)
-    {
-        this.range = range;
-    }
-
     public double getPercentage()
     {
         return percentage;
-    }
-
-    public void setPercentage(double percentage)
-    {
-        this.percentage = percentage;
     }
 
     public double getOutcome()
@@ -55,9 +51,8 @@ public class CoinDrop
         CoinDrop coin = null;
 
         if (config != null && config.contains("Coin_Maximum") && config.contains("Coin_Minimum") && config.contains("Coin_Percent")) {
-            coin = new CoinDrop();
-            coin.setRange(new DoubleRange(config.getDouble("Coin_Minimum", 0), config.getDouble("Coin_Maximum", 0)));
-            coin.setPercentage(config.getDouble("Coin_Percent", 0.0D));
+            coin = new CoinDrop(new DoubleRange(config.getDouble("Coin_Minimum", 0), config.getDouble("Coin_Maximum", 0)),
+                    config.getDouble("Coin_Percent", 0.0D));
         }
 
         return coin;
