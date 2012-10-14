b03e89776fd7486e0624a0adaa64a695e916372d
mung3r <rodney@bighatchet.com>
2012-10-13 23:59:13 -0700

    minor code improvement

--- src/main/java/se/crafted/chrisb/ecoCreature/commands/Command.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/commands/Command.java
@@ -6,32 +6,32 @@ import org.bukkit.command.CommandSender;
 
 public interface Command {
 
-    public void cancelInteraction(CommandSender executor);
+    void cancelInteraction(CommandSender executor);
 
-    public boolean execute(CommandSender executor, String identifier, String[] args);
+    boolean execute(CommandSender executor, String identifier, String[] args);
 
-    public String getDescription();
+    String getDescription();
 
-    public List<String> getIdentifiers();
+    List<String> getIdentifiers();
 
-    public int getMaxArguments();
+    int getMaxArguments();
 
-    public int getMinArguments();
+    int getMinArguments();
 
-    public String getName();
+    String getName();
 
-    public List<String> getNotes();
+    List<String> getNotes();
 
-    public String getPermission();
+    String getPermission();
 
-    public String getUsage();
+    String getUsage();
 
-    public boolean isIdentifier(CommandSender executor, String input);
+    boolean isIdentifier(CommandSender executor, String input);
 
-    public boolean isInProgress(CommandSender executor);
+    boolean isInProgress(CommandSender executor);
 
-    public boolean isInteractive();
+    boolean isInteractive();
 
-    public boolean isShownOnHelpMenu();
+    boolean isShownOnHelpMenu();
 
 }
