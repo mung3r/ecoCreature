7e26db26ae993b057cba21f7d1dde8fbd8c4ce9e
mung3r <rodney@bighatchet.com>
2013-01-14 23:57:03 -0800

    issue #115: fixed bug with spawn camp exp orb drops

--- src/main/java/se/crafted/chrisb/ecoCreature/events/PlayerKilledEvent.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/events/PlayerKilledEvent.java
@@ -22,35 +22,87 @@ package se.crafted.chrisb.ecoCreature.events;
 import java.util.List;
 
 import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
 import se.crafted.chrisb.ecoCreature.commons.EntityUtils;
 
-public final class PlayerKilledEvent extends PlayerDeathEvent
+public final class PlayerKilledEvent extends Event
 {
+    private static final HandlerList handlers = new HandlerList();
+
+    private PlayerDeathEvent event;
+
     public static PlayerKilledEvent createEvent(PlayerDeathEvent event)
     {
-        return new PlayerKilledEvent(event.getEntity(), event.getDrops(), event.getDroppedExp(), event.getNewExp(), event.getNewTotalExp(), event.getNewLevel(), event.getDeathMessage());
+        return new PlayerKilledEvent(event);
     }
 
-    private PlayerKilledEvent(Player player, List<ItemStack> drops, int droppedExp, int newExp, int newTotalExp, int newLevel, String deathMessage)
+    private PlayerKilledEvent(PlayerDeathEvent event)
     {
-        super(player, drops, droppedExp, newExp, newTotalExp, newLevel, deathMessage);
+        this.event = event;
     }
 
     public Player getVictim()
     {
-        return getEntity();
+        return event.getEntity();
     }
 
     public Player getKiller()
     {
-        return getEntity().getKiller();
+        return event.getEntity().getKiller();
     }
 
     public String getWeaponName()
     {
         return EntityUtils.getItemNameInHand(getKiller());
+    }
+
+    public Player getEntity()
+    {
+        return event.getEntity();
+    }
+
+    public List<ItemStack> getDrops()
+    {
+        return event.getDrops();
+    }
+
+    public int getDroppedExp()
+    {
+        return event.getDroppedExp();
+    }
+
+    public void setDroppedExp(int exp)
+    {
+        event.setDroppedExp(exp);
+    }
+
+    public int getNewExp()
+    {
+        return event.getNewExp();
+    }
+
+    public int getNewTotalExp()
+    {
+        return event.getNewTotalExp();
+    }
+
+    public int getNewLevel()
+    {
+        return event.getNewLevel();
+    }
+
+    public String getDeathMessage()
+    {
+        return event.getDeathMessage();
+    }
+
+    @Override
+    public HandlerList getHandlers()
+    {
+        return handlers;
     }
 }
