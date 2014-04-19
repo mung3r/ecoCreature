a8bb22492f909ce9a0a39a89f4b906f21df227ca
mung3r <rodney@bighatchet.com>
2014-04-19 00:33:14 -0700

    removed unused import

--- src/main/java/se/crafted/chrisb/ecoCreature/rewards/models/JockeyDrop.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/rewards/models/JockeyDrop.java
@@ -20,7 +20,6 @@
 package se.crafted.chrisb.ecoCreature.rewards.models;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
