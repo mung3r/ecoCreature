a8a50c46b5d659d3ee0d1c50db290610e224fe2b
mung3r <rodney@bighatchet.com>
2014-06-12 23:40:34 -0700

    revert last logging change

--- src/main/java/se/crafted/chrisb/ecoCreature/commons/LoggerUtil.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/commons/LoggerUtil.java
@@ -87,7 +87,7 @@ public final class LoggerUtil
     {
         if (debug) {
             @SuppressWarnings("restriction")
-            Class<?> aClass = sun.reflect.Reflection.getCallerClass();
+            Class<?> aClass = sun.reflect.Reflection.getCallerClass(2);
 
             logger.info(format(aClass.getSimpleName() + ": " + msg));
         }
