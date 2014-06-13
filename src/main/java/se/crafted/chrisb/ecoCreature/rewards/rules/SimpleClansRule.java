adb9eeb1f205ae3261be0168fbd4b622cea7567d
mung3r <mung3r@bighatchet.com>
2014-06-13 12:21:51 -0700

    check for null clanplayer

--- src/main/java/se/crafted/chrisb/ecoCreature/rewards/rules/SimpleClansRule.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/rewards/rules/SimpleClansRule.java
@@ -24,11 +24,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.bukkit.configuration.ConfigurationSection;
-import org.bukkit.entity.Player;
 
 import net.sacredlabyrinth.phaed.simpleclans.ClanPlayer;
 import net.sacredlabyrinth.phaed.simpleclans.SimpleClans;
-import net.sacredlabyrinth.phaed.simpleclans.managers.ClanManager;
 import se.crafted.chrisb.ecoCreature.commons.DependencyUtils;
 import se.crafted.chrisb.ecoCreature.events.PlayerKilledEvent;
 
@@ -53,10 +51,8 @@ public class SimpleClansRule extends AbstractPlayerRule
 
         if (DependencyUtils.hasSimpleClans())
         {
-            ClanManager clanManager = SimpleClans.getInstance().getClanManager();
-            Player killer = event.getKiller();
-            ClanPlayer victim = clanManager.getClanPlayer(event.getVictim());
-            ruleBroken = !victim.isRival(killer);
+            ClanPlayer killer = SimpleClans.getInstance().getClanManager().getClanPlayer(event.getKiller());
+            ruleBroken = killer != null && !killer.isRival(event.getVictim());
         }
 
         return ruleBroken;
