d6f6bf365ef4048892e27dd2d1e3628ea38f5bb8
Rodney Ramos <rodney.ramos@leadpoint.com>
2015-07-10 11:37:10 -0700

    changed replace attribute flag to true

--- src/main/java/se/crafted/chrisb/ecoCreature/drops/chances/ItemChance.java
+++ src/main/java/se/crafted/chrisb/ecoCreature/drops/chances/ItemChance.java
@@ -141,7 +141,7 @@ public class ItemChance extends AbstractChance implements DropChance
                     }
                 }
                 itemStack.addUnsafeEnchantments(EnchantmentChance.nextEnchantments(enchantments));
-                Attributes.apply(itemStack, attributes, false);
+                Attributes.apply(itemStack, attributes, true);
 
                 if (itemStack.getAmount() > 0) {
                     return itemStack;
